b5edb5779a58e0aeb47ca9646f7d53178895ccde
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-22 04:43:59 -0600

    cards fixed.

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
- import './GigCard.scss'
-import { Link } from "react-router-dom"
-const GigCard = ({item}) => {
-  return (
-    <Link to="/gig/2" >
-    <div className="gigCard" >
-      <img src={item.img} alt="" />
-      <div className="info">
-        <div className="user">
-          <img src={item.pp} alt="" />
-          <span>{item.username}</span>
-        </div>
-        <p>{item.desc}</p>
-        <div className="star">
-          <img src="public\img\star.png" alt="" />
-          <span>{item.star}</span>
-        </div>
-      </div>
-      <div className="details">
-        <img src="public\img\heart.png" alt="" />
-        <span>Starting at</span>
-        <h2>${item.price}</h2>
-      </div>
-    </div>
-    </Link>
-  )
-}
+import React from "react";
+import "./GigCard.scss";
+import { Link } from "react-router-dom";
 
-export default GigCard
+const GigCard = ({ item }) => {
+  return (
+    <Link to="/gig/123" className="link">
+      <div className="gigCard">
+        <img src={item.img} alt="" />
+        <div className="info">
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <img src="./img/star.png" alt="" />
+            <span>{item.star}</span>
+          </div>
+        </div>
+        <hr />
+        <div className="detail">
+          <img src="./img/heart.png" alt="" />
+          <div className="price">
+            <span>STARTING AT</span>
+            <h2>
+              $ {item.price}
+              <sup>99</sup>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default GigCard;
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,11 +1,58 @@
-
-
-import React from 'react'
-import "./Gigs.scss"
+import React, { useState } from "react";
+import "./Gigs.scss";
+import { gigs } from "../../data";
+import GigCard from "../../components/gigCard/GigCard.jsx";
 const Gigs = () => {
-  return (
-    <div>Gigs</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState('sales')
 
-export default Gigs
+  const reSort = (type) => {
+    setSort(type)
+    setOpen(false)
+  }
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">HIVES SOFTWARE DEVELOPER</span>
+        <h1>Next warrior</h1>
+        <p>
+          Explore the boundaries of software development with hives developers
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort By</span>
+            <span className="sortType">{sort ==="sales" ? 'Best selling' : 'Newest'}</span>
+            <img
+              src="public\img\down.png"
+              alt=""
+              onClick={() => setOpen(!open)}
+            />
+            {open && (
+              <div className="rightMenu">
+                    {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                  )}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gigs;
