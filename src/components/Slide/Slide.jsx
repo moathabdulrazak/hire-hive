0fee3463a72f637d86bbd435eaabd5bc25086223
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-22 02:49:12 -0600

    slider working.

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,26 +1,16 @@
 import React from 'react'
 import './Slide.scss'
 import  { Slider } from "infinite-react-carousel"
+import CatCard from "../catCard/catCard.jsx"
+import {cards} from '../../data.js'
 const Slide = () => {
   return (
     <div className="slide" >
       <div className="container">
-      <Slider dots>
-    <div>
-      <h3>1</h3>
-    </div>
-    <div>
-      <h3>2</h3>
-    </div>
-    <div>
-      <h3>3</h3>
-    </div>
-    <div>
-      <h3>4</h3>
-    </div>
-    <div>
-      <h3>5</h3>
-    </div>
+      <Slider>
+      {cards.map(card => (
+        <CatCard card={card} key={card.id} />
+      ))  }
   </Slider>
       </div>
     </div>
--- src/components/catCard/catCard.jsx
+++ src/components/catCard/catCard.jsx
@@ -6,9 +6,9 @@ function CatCard({ card }) {
   return (
     <Link to="/gigs?cat=design">
       <div className="catCard">
-        <img src={card.img} alt="" />
-        <span className="desc">{card.desc}</span>
-        <span className="title">{card.title}</span>
+        <img src={card?.img} alt="" />
+        <span className="desc">{card?.desc}</span>
+        <span className="title">{card?.title}</span>
       </div>
     </Link>
   );
