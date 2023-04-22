11fc501512b85da7a56cde9f3d1725a0dca03500
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-22 02:56:46 -0600

    slider passed as props.

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import './Slide.scss'
 import  { Slider } from "infinite-react-carousel"
 import CatCard from "../catCard/catCard.jsx"
-import {cards} from '../../data.js'
-const Slide = () => {
+const Slide = ({children, slideToShow,arrowScroll }) => {
   return (
     <div className="slide" >
       <div className="container">
-      <Slider>
-      {cards.map(card => (
-        <CatCard card={card} key={card.id} />
-      ))  }
+      <Slider arrowScroll={arrowScroll} slidesToShow={slideToShow} >
+   {children}
   </Slider>
       </div>
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,14 +4,19 @@ import Featured from "../../components/featured/Featured.jsx"
 import TrustedBy from "../../components/trustedBy/TrustedBy.jsx"
 import Slide from "../../components/Slide/Slide.jsx"
 
-
+import {cards} from '../../data.js'
+import CatCard from "../../components/catCard/catCard.jsx"
 const Home = () => {
   return (
     <div className="home" >
 
       <Featured/>
       <TrustedBy/>
-      <Slide/>
+      <Slide slideToShow={5} arrowScroll={5} >
+        {cards.map(card => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   )
 }
