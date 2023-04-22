679c3887bb82c209f9d5f0e4e4a4695aa481e079
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-22 06:08:16 -0600

    messages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Login from "./pages/login/Login";
 import Add from "./pages/add/Add";
 import Footer from "./components/footer/Footer";
 import { Register } from "./pages/register/Register";
-import { Messages } from "./pages/messages/messages";
+import Messages  from "./pages/messages/messages";
 import { Message } from "./pages/message/message";
 import Orders from "./pages/orders/Orders.jsx";
 
