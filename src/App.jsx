715816fd45ad31aac4b9c8b97c125ee231b16c25
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-22 01:28:26 -0600

    scroll effect on nav bar works

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ function App(){
   return (
     <div>
       <Navbar/>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reiciendis excepturi fugit, atque inventore nam nihil eum optio maxime ut corporis totam. Velit adipisci repellendus enim vitae nihil. Facere, nemo! </h1>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import './Navbar.scss'
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener('scroll', isActive)
+    }
+  }, []);
+
   return (
-    <div className="navbar" >
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-          <span className="text" >Hire Hive</span>
-          <span className="dot" >.</span>
+          {/* <Link to={'/'} > */}
+          <span className="text">Hire Hive</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
         </div>
         <div className="links">
-        <span>Hives business</span>
-        <span>Explore</span>
-        <span>English</span>
-        <span>Sign in</span>
-        <span>Become a seller</span>
-        <button>Join</button>
+          <span>Hives business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a seller</span>
+          <button>Join</button>
         </div>
       </div>
-
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
