d15ddc716a25232a0c58bbd1402d0602a3b0f53f
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-22 03:47:44 -0600

    footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
-import './Footer.scss'
-const Footer = () => {
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
   return (
-    <div className="footer" >Footer</div>
-  )
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Liverr</span>
+            <span>Buying on Liverr</span>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Events</span>
+            <span>Blog</span>
+            <span>Influencers</span>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className="item">
+            <h2>More From Fiverr</h2>
+            <span>Liverr Business</span>
+            <span>Liverr Pro</span>
+            <span>Liverr Logo Maker</span>
+            <span>Liverr Guides</span>
+            <span>Get Inspired</span>
+            <span>Liverr Select</span>
+            <span>ClearVoice</span>
+            <span>Liverr Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>Hire Hive</h2>
+            <span>© Hire Hive 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+           <a href="https://www.linkedin.com/in/moathabdulrazak/" target="_blank"> <img  src="public\img\linkedin.png" alt="" /></a>   
+            </div>
+            <div className="social">
+           <a href="https://github.com/moathabdulrazak" target="_blank"> <img  src="public\img\icons8-github-50.png" alt="" /></a>   
+            </div>
+            <div className="link">
+              <img src="public\img\language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="public\img\coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="public\img\accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
