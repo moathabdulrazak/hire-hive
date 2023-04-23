e33c23110e48eee7ad8819f788ba2f2c3242895c
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-23 03:18:48 -0600

    cookies, jwt.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,39 +1,43 @@
-import User from '../models/user.model.js'
-import bcrypt from "bcrypt"
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-
-export const register  = async (req, res) => {
+export const register = async (req, res) => {
   try {
-    const hash = bcrypt.hashSync(req.body.password, 10)
+    const hash = bcrypt.hashSync(req.body.password, 10);
     const newUser = new User({
       ...req.body,
       password: hash,
-    })
+    });
 
     await newUser.save();
-    res.status(201).send('user has been created')
+    res.status(201).send("user has been created");
   } catch (error) {
-    res.status(500).send("something went wrong with register")
+    res.status(500).send("something went wrong with register");
   }
-}
-
-
+};
 
 export const login = async (req, res) => {
   try {
-    const user = await User.findOne({username: req.body.username})
-    if(!user) return res.status(404).send("User Not found!")
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User Not found!");
 
-    const isCorrect = bcrypt.compareSync(req.body.password, user.password)
-    if(!isCorrect) return res.status(400).send("Wrong password or username!")
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(400).send("Wrong password or username!");
+
+    const token = jwt.sign({
+      id: user._id,
+      isSeller: user.isSeller,
+    }, process.env.JWT_KEY );
 
     // @ts-ignore
-    const {password, ...info} = user._doc
+    const { password, ...info } = user._doc;
 
-    res.status(200).send(info)
+    res.cookie("accessToken", token, {
+      httpOnly: "true"
+    }).status(200).send(info);
   } catch (error) {
-    res.status(500).send("something went wrong with logging in")
+    res.status(500).send("something went wrong with logging in");
   }
-}
-export const logout = (req, res) => {
-}
+};
+export const logout = (req, res) => {};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import { deleteUser } from "../controllers/user.controller.js";
 
 
 const router = express.Router()
 
 
-router.get("/register")
-router.get("/login")
+router.delete("/:id", deleteUser)
 
 export default router;
