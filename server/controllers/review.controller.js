c1d431ce564f3324467612459df82fedc9c0c587
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-24 05:15:45 -0600

    s

--- server/controllers/review.controller.js
+++ server/controllers/review.controller.js
@@ -19,10 +19,10 @@ export const createReview = async (req, res, next) => {
       userId: req.userId,
     });
 
-    // if (review)
-    //   return next(
-    //     createError(403, "You have already created a review for this gig!")
-    //   );
+    if (review)
+      return next(
+        createError(403, "You have already created a review for this gig!")
+      );
 
     //TODO: check if the user purchased the gig.
 
