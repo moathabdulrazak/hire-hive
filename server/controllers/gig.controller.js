f78af94dc3003f2918b0432cf8ac879c17d8d17b
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-24 00:37:20 -0600

    Delete gig BE works.

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -18,11 +18,35 @@ export const createGig = async (req, res, next) => {
   }
 }
 
+
 export const deleteGig = async (req, res, next) => {
+
+  try {
+    const gig = await Gig.findById(req.params.id)
+
+    // @ts-ignore
+    if(gig.userId !== req.userId ) return next(createError(403, "You can only delete your gigs."))
+
+    await Gig.findByIdAndDelete(req.params.id)
+    // @ts-ignore
+    res.status(200).send(`Gig at this Id ${gig.name} has been deleted`)
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const getGig = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const getGigs = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
 }
