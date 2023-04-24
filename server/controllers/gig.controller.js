88e6f0c4796ad9501541cd8ee2593cc0e0ccf1c9
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-23 23:39:20 -0600

    setting up.

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -1,5 +1,14 @@
+import Gig from '../models/gig.model.js'
+import createError from "../utils/createError.js"
 
+export const createGig = async (req, res, next) => {
+}
 
+export const deleteGig = async (req, res, next) => {
+}
 
-export const fn = (req, res) => {
+export const getGig = async (req, res, next) => {
+}
+
+export const getGigs = async (req, res, next) => {
 }
